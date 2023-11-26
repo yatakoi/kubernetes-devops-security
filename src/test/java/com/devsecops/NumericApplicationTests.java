83aab715693b83419edafa339cc707dac77dc431
Maksim <allkall@yandex.ru>
2023-11-26 13:39:55 +1000

    xxx

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -36,7 +36,8 @@ public class NumericApplicationTests {
 
     @Test
     public void smallerThanOrEqualToFiftyMessage() throws Exception {
-        this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("Smaller than or equal to 50"));
     }
 
     @Test
@@ -45,16 +46,10 @@ public class NumericApplicationTests {
                 .andExpect(content().string("Greater than 50"));
     }
     
-@Test
-    public void welcomeMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Kubernetes DevSecOps"));
-    }
-
     @Test
-    public void incrementByOneMessage() throws Exception {
-         this.mockMvc.perform(get("/increment/50")).andDo(print()).andExpect(status().isOk())
-           .andExpect(content().string("51"));
-    }           
+    public void welcomeMessage() throws Exception {
+         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    }
+    
 
 }

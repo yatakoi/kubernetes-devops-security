0958a34a7bd787670dd3f9731914002be6337df4
Maksim <allkall@yandex.ru>
2023-11-26 13:05:52 +1000

    xxx

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -52,9 +52,4 @@ public class NumericApplicationTests {
     }
     
 
-    @Test
-    public void incrementByOneMessage() throws Exception {
-         this.mockMvc.perform(get("/increment/50")).andDo(print()).andExpect(status().isOk())
-           .andExpect(content().string("51"));
-
 }

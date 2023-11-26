501c2e6e77afbe1ca60d4384f37a6bd1ade2cfd5
Maksim <allkall@yandex.ru>
2023-11-26 13:36:02 +1000

    xxx

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -36,7 +36,7 @@ public class NumericApplicationTests {
 
     @Test
     public void smallerThanOrEqualToFiftyMessage() throws Exception {
-        this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk());
     }
 
     @Test

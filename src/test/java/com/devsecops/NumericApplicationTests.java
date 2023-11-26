aa86e39d571aa3a53cbc66f8df756ef44cd7e0f1
Maksim <allkall@yandex.ru>
2023-11-26 13:43:36 +1000

    xxx

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -49,6 +49,7 @@ public class NumericApplicationTests {
     @Test
     public void welcomeMessage() throws Exception {
          this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+                .andExpect(content().string("Kubernetes DevSecOps"));
     }
     
 

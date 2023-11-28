c90f5f44b53b5aaf2bbaaaf688fdfd1972d91545
Maksim <allkall@yandex.ru>
2023-11-28 23:42:23 +1000

    xxx

--- zap.sh
+++ zap.sh
@@ -6,7 +6,7 @@ PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].node
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
 #docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap-rules -w report.md -J json_report.json -r zap_report.html
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -w report.md -J json_report.json -r zap_report.html
 exit_code=$?
 
 # comment above cmd and uncomment below lines to run with CUSTOM RULES
